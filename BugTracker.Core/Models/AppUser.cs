bdcc4b07915c967df0a17666ae3b01009bc0d6b4
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 09:47:47 -0500

    Add nav props to appuser
    Added userprojects and notifications collections

--- BugTracker.Core/Models/AppUser.cs
+++ BugTracker.Core/Models/AppUser.cs
@@ -8,6 +8,9 @@ namespace BugTracker.Core.Models
     public class AppUser : IdentityUser<int>
     {
         public string FirstName { get; set; }
-        public string LastName { get; set; }    
+        public string LastName { get; set; }
+
+        public virtual ICollection<UserProject> UserProjects { get; set; }
+        public virtual ICollection<Notification> Notifications { get; set; }
     }
 }
