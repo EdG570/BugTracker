7fa3343b2fbe8203746d112e0b9ac789c9fb08ff
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 09:43:06 -0500

    Add userproject model

---
+++ BugTracker.Core/Models/UserProject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTracker.Core.Models
+{
+    public class UserProject
+    {
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+    }
+}
