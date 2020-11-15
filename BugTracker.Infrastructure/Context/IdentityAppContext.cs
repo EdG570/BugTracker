b2b6aa8be05c96e75823b1c91814fac4a864a108
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 11:18:03 -0500

    Add seeds migration to db

--- BugTracker.Infrastructure/Context/IdentityAppContext.cs
+++ BugTracker.Infrastructure/Context/IdentityAppContext.cs
@@ -1,4 +1,6 @@
 ﻿using BugTracker.Core.Models;
+using BugTracker.Core.Models.Enums;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -27,6 +29,246 @@ namespace BugTracker.Infrastructure.Context
             base.OnModelCreating(builder);
 
             builder.Entity<UserProject>().HasKey(tu => new { tu.ProjectId, tu.AppUserId });
+
+            /* ------------------------------- SEEDS -------------------------------- */
+
+            //// Seed Users
+            //PasswordHasher<AppUser> ph = new PasswordHasher<AppUser>();
+            //PasswordHasher<AppUser> ph2 = new PasswordHasher<AppUser>();
+
+            //AppUser appUser = new AppUser
+            //{
+            //    Id = 1,
+            //    UserName = "user@email.com",
+            //    NormalizedUserName = "USER@EMAIL.COM",
+            //    Email = "user@email.com",
+            //    NormalizedEmail = "USER@EMAIL.COM",
+            //    FirstName = "John",
+            //    LastName = "Smith",
+            //    PasswordHash = ph.HashPassword(null, "Pass123!"),
+            //    SecurityStamp = Guid.NewGuid().ToString(),
+            //    ConcurrencyStamp = Guid.NewGuid().ToString("D")
+
+            //};
+
+            //AppUser appUser2 = new AppUser
+            //{
+            //    Id = 2,
+            //    UserName = "user2@email.com",
+            //    NormalizedUserName = "USER2@EMAIL.COM",
+            //    Email = "user2@email.com",
+            //    NormalizedEmail = "USER2@EMAIL.COM",
+            //    FirstName = "Billy",
+            //    LastName = "Bobson",
+            //    PasswordHash = ph2.HashPassword(null, "Pass234!"),
+            //    ConcurrencyStamp = Guid.NewGuid().ToString("D"),
+            //    SecurityStamp = Guid.NewGuid().ToString()
+            //};
+
+            //builder.Entity<AppUser>().HasData(
+            //    appUser,
+            //    appUser2
+            //);
+
+            //// Seed Projects
+            //builder.Entity<Project>().HasData(
+            //    new Project
+            //    {
+            //        Id = 1,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Description = "A web application for managing staffing",
+            //        Name = "Staffing App",
+            //        OwnerId = 1
+            //    },
+            //    new Project
+            //    {
+            //        Id = 2,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Description = "A web application for managing quality control",
+            //        Name = "Qc App",
+            //        OwnerId = 1
+            //    },
+            //    new Project
+            //    {
+            //        Id = 3,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Description = "An internal web app for measuring agent performance",
+            //        Name = "Scorecard App",
+            //        OwnerId = 1
+            //    },
+            //    new Project
+            //    {
+            //        Id = 4,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Description = "A web application for agents to monitor performance",
+            //        Name = "Scoreboard App",
+            //        OwnerId = 1
+            //    },
+            //    new Project
+            //    {
+            //        Id = 5,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Description = "A web app for error tracking",
+            //        Name = "Error Tracker",
+            //        OwnerId = 1
+            //    }
+            //);
+
+            //// Seed Tickets
+            //builder.Entity<Ticket>().HasData(
+            //    new Ticket
+            //    {
+            //        Id = 1,
+            //        Title = "Joe Borski's team will not update",
+            //        Description = "I've attempted to update his team numerous times but to no avail. Can you take a look?",
+            //        AssignedToId = 1,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Priority = Priority.High,
+            //        Status = Status.Open,
+            //        ProjectId = 1,
+            //        RequestorId = 1,
+            //        Type = TicketType.Bug
+            //    },
+            //    new Ticket
+            //    {
+            //        Id = 2,
+            //        Title = "Susan Jones isn't showing up for this month",
+            //        Description = "Can you check why she isn't loading for this month?",
+            //        AssignedToId = 1,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Priority = Priority.High,
+            //        Status = Status.Open,
+            //        ProjectId = 1,
+            //        RequestorId = 1,
+            //        Type = TicketType.Bug
+            //    },
+            //    new Ticket
+            //    {
+            //        Id = 3,
+            //        Title = "Filter by employee last name",
+            //        Description = "We'd like to filter by employee last name. Is this something you guys can add to the app?",
+            //        AssignedToId = 1,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Priority = Priority.Low,
+            //        Status = Status.Closed,
+            //        ProjectId = 1,
+            //        RequestorId = 1,
+            //        Type = TicketType.Feature
+            //    },
+            //    new Ticket
+            //    {
+            //        Id = 4,
+            //        Title = "The app is down!",
+            //        Description = "Staffing is due by close of business and noone can access the app.",
+            //        AssignedToId = 2,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Priority = Priority.Urgent,
+            //        Status = Status.InProgress,
+            //        ProjectId = 1,
+            //        RequestorId = 1,
+            //        Type = TicketType.Bug
+            //    },
+            //    new Ticket
+            //    {
+            //        Id = 5,
+            //        Title = "Is this app working?",
+            //        Description = "I can't load the staffing app.",
+            //        AssignedToId = 0,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Priority = Priority.Urgent,
+            //        Status = Status.Open,
+            //        ProjectId = 1,
+            //        RequestorId = 1,
+            //        Type = TicketType.Bug
+            //    },
+            //    new Ticket
+            //    {
+            //        Id = 6,
+            //        Title = "Only 5 records for today",
+            //        Description = "There are only 5 records to qc for today. Is this correct?",
+            //        AssignedToId = 2,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Priority = Priority.High,
+            //        Status = Status.InProgress,
+            //        ProjectId = 2,
+            //        RequestorId = 2,
+            //        Type = TicketType.Bug
+            //    },
+            //    new Ticket
+            //    {
+            //        Id = 7,
+            //        Title = "Add a check for total records",
+            //        Description = "We'll need to add a check to see if all the records are being pulled in from the call center.",
+            //        AssignedToId = 0,
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        Priority = Priority.Medium,
+            //        Status = Status.Open,
+            //        ProjectId = 2,
+            //        RequestorId = 2,
+            //        Type = TicketType.Task
+            //    }
+            //);
+
+            //builder.Entity<Comment>().HasData(
+            //    new Comment
+            //    {
+            //        Id = 1,
+            //        TicketId = 1,
+            //        Description = "I'm troubleshooting this now",
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "John Smith",
+            //        CreatorId = 1
+            //    },
+            //    new Comment
+            //    {
+            //        Id = 2,
+            //        TicketId = 1,
+            //        Description = "Ok let me know what you find!",
+            //        CreatedAt = DateTime.Now,
+            //        CreatedBy = "Billy Bobson",
+            //        CreatorId = 2
+            //    }
+            //);
+
+            //builder.Entity<UserProject>().HasData(
+            //    new UserProject
+            //    {
+            //        AppUserId = 1,
+            //        ProjectId = 1
+            //    },
+            //    new UserProject
+            //    {
+            //        AppUserId = 1,
+            //        ProjectId = 2
+            //    },
+            //    new UserProject
+            //    {
+            //        AppUserId = 1,
+            //        ProjectId = 3
+            //    },
+            //    new UserProject
+            //    {
+            //        AppUserId = 1,
+            //        ProjectId = 4
+            //    },
+            //    new UserProject
+            //    {
+            //        AppUserId = 1,
+            //        ProjectId = 5
+            //    }
+            //);
 
         }
 
---
+++ BugTracker.Infrastructure/Migrations/20201115161447_AddSeeds.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BugTracker.Infrastructure.Migrations
+{
+    public partial class AddSeeds : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[,]
+                {
+                    { 1, 0, "b9cf14db-5dcb-471b-8554-b17764ad9dd7", "user@email.com", false, "John", "Smith", false, null, "USER@EMAIL.COM", "USER@EMAIL.COM", "AQAAAAEAACcQAAAAEK1U6hZfE0/yJG/0dTqFCuNpxWaDqWCCCJP8bP1SbRUWw1Y1cCRn2t1WVn8SaXfrxw==", null, false, "414b4496-809a-425b-85ce-ac33405fd4a1", false, "user@email.com" },
+                    { 2, 0, "b6128f32-088a-4c1c-9304-b7d6a9db1011", "user2@email.com", false, "Billy", "Bobson", false, null, "USER2@EMAIL.COM", "USER2@EMAIL.COM", "AQAAAAEAACcQAAAAEKgl1VGGZZQDVVEWFqfVYNc9T2172S1XFWgeNm7+D0LsbdlvMFP3nz2gfOuQEMySDQ==", null, false, "10f723ea-ec54-4ed9-84af-7ee42fcce7aa", false, "user2@email.com" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Projects",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "Description", "Name", "OwnerId", "RepositoryUri", "UpdatedAt", "UpdatedBy" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2020, 11, 15, 11, 14, 46, 568, DateTimeKind.Local).AddTicks(9233), "John Smith", "A web application for managing staffing", "Staffing App", 1, null, null, null },
+                    { 2, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(935), "John Smith", "A web application for managing quality control", "Qc App", 1, null, null, null },
+                    { 3, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(971), "John Smith", "An internal web app for measuring agent performance", "Scorecard App", 1, null, null, null },
+                    { 4, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(976), "John Smith", "A web application for agents to monitor performance", "Scoreboard App", 1, null, null, null },
+                    { 5, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(979), "John Smith", "A web app for error tracking", "Error Tracker", 1, null, null, null }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Tickets",
+                columns: new[] { "Id", "AssignedToId", "CreatedAt", "CreatedBy", "Description", "Priority", "ProjectId", "RequestorId", "Status", "Title", "Type" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(2883), "John Smith", "I've attempted to update his team numerous times but to no avail. Can you take a look?", 2, 1, 1, 0, "Joe Borski's team will not update", 2 },
+                    { 2, 1, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(4803), "John Smith", "Can you check why she isn't loading for this month?", 2, 1, 1, 0, "Susan Jones isn't showing up for this month", 2 },
+                    { 3, 1, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(4858), "John Smith", "We'd like to filter by employee last name. Is this something you guys can add to the app?", 0, 1, 1, 2, "Filter by employee last name", 1 },
+                    { 4, 2, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(4863), "John Smith", "Staffing is due by close of business and noone can access the app.", 3, 1, 1, 1, "The app is down!", 2 },
+                    { 5, 0, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(4867), "John Smith", "I can't load the staffing app.", 3, 1, 1, 0, "Is this app working?", 2 },
+                    { 6, 2, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(4871), "John Smith", "There are only 5 records to qc for today. Is this correct?", 2, 2, 2, 1, "Only 5 records for today", 2 },
+                    { 7, 0, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(4875), "John Smith", "We'll need to add a check to see if all the records are being pulled in from the call center.", 1, 2, 2, 0, "Add a check for total records", 0 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "UserProjects",
+                columns: new[] { "AppUserId", "ProjectId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 1, 2 },
+                    { 1, 3 },
+                    { 1, 4 },
+                    { 1, 5 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Comments",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "CreatorId", "Description", "TicketId" },
+                values: new object[] { 1, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(6336), "John Smith", 1, "I'm troubleshooting this now", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Comments",
+                columns: new[] { "Id", "CreatedAt", "CreatedBy", "CreatorId", "Description", "TicketId" },
+                values: new object[] { 2, new DateTime(2020, 11, 15, 11, 14, 46, 569, DateTimeKind.Local).AddTicks(7201), "Billy Bobson", 2, "Ok let me know what you find!", 1 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "UserProjects",
+                keyColumns: new[] { "AppUserId", "ProjectId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "UserProjects",
+                keyColumns: new[] { "AppUserId", "ProjectId" },
+                keyValues: new object[] { 1, 2 });
+
+            migrationBuilder.DeleteData(
+                table: "UserProjects",
+                keyColumns: new[] { "AppUserId", "ProjectId" },
+                keyValues: new object[] { 1, 3 });
+
+            migrationBuilder.DeleteData(
+                table: "UserProjects",
+                keyColumns: new[] { "AppUserId", "ProjectId" },
+                keyValues: new object[] { 1, 4 });
+
+            migrationBuilder.DeleteData(
+                table: "UserProjects",
+                keyColumns: new[] { "AppUserId", "ProjectId" },
+                keyValues: new object[] { 1, 5 });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Projects",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
