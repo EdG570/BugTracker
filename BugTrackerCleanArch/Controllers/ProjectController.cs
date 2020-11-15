772e40f7c34eecf4750fa18eeac51c23870a47c1
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 14:00:48 -0500

    Add project controller index action

--- BugTrackerCleanArch/Controllers/ProjectController.cs
+++ BugTrackerCleanArch/Controllers/ProjectController.cs
@@ -1,16 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using AutoMapper;
+using BugTracker.Application.ViewModels.Project;
+using BugTracker.Core.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+
 
 namespace BugTracker.Application.Controllers
 {
     public class ProjectController : Controller
     {
-        public IActionResult Index()
+        private readonly IAppUserService _appUserService;
+        private readonly IProjectService _projectService;
+        private readonly IUserProjectService _userProjectService;
+        private readonly IMapper _mapper;
+
+        public ProjectController(IAppUserService appUserService, IProjectService projectService, IUserProjectService userProjectService, IMapper mapper)
         {
-            return View();
+            _appUserService = appUserService;
+            _projectService = projectService;
+            _userProjectService = userProjectService;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var userId = Convert.ToInt32(User.FindFirstValue(ClaimTypes.NameIdentifier));
+            var user = await _appUserService.FindOne(userId);
+
+            return View(new IndexViewModel { User = user });
         }
     }
 }
