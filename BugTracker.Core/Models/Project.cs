3bbaba803738b7b0c4107cd812cf8137e1c3c10e
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 09:23:44 -0500

    Add project model

---
+++ BugTracker.Core/Models/Project.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BugTracker.Core.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        public string Description { get; set; }
+
+        [Url]
+        public string RepositoryUri { get; set; }
+
+        [Required]
+        public string CreatedBy { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        public string UpdatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+
+        public int OwnerId { get; set; }
+
+    }
+}
