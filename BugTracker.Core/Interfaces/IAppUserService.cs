df99856f8e9b61b4f9df4076bacb7e44a4ba1dcc
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 10:26:59 -0500

    Add appuser service

---
+++ BugTracker.Core/Interfaces/IAppUserService.cs
@@ -0,0 +1,13 @@
+﻿using BugTracker.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Core.Interfaces
+{
+    public interface IAppUserService
+    {
+        Task<AppUser> FindOne(int id);
+    }
+}
---
+++ BugTracker.Core/Services/AppUserService.cs
@@ -0,0 +1,25 @@
+﻿using BugTracker.Core.Interfaces;
+using BugTracker.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Core.Services
+{
+    public class AppUserService : IAppUserService
+    {
+        private readonly IAppUserRepository _appUserRepo;
+
+        public AppUserService(IAppUserRepository appUserRepo)
+        {
+            _appUserRepo = appUserRepo;
+        }
+
+        public async Task<AppUser> FindOne(int id)
+        {
+            return await _appUserRepo.FindOne(id);
+        }
+
+    }
+}
