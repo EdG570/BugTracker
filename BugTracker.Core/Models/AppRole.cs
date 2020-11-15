e492cc5c0743f19e5dff01655721c52a33acd976
Ed Glazenski <eglazenski@gmail.com>
2020-11-14 20:54:24 -0500

    Add appuser and approle models
    AppUser and AppRole models added for identity
    customization.

---
+++ BugTracker.Core/Models/AppRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTracker.Core.Models
+{
+    public class AppRole : IdentityRole<int>
+    {
+    }
+}
