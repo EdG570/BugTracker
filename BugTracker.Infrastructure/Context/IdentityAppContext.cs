f7362a5655d18d5120dae6fd105987c5d2746f6e
Ed Glazenski <eglazenski@gmail.com>
2020-11-14 21:04:46 -0500

    Add db context

---
+++ BugTracker.Infrastructure/Context/IdentityAppContext.cs
@@ -0,0 +1,22 @@
+﻿using BugTracker.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTracker.Infrastructure.Context
+{
+    public class IdentityAppContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+
+        public IdentityAppContext(DbContextOptions<IdentityAppContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppRole> AppRoles { get; set; }
+        
+    }
+}
