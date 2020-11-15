3a0bba12dc7a1577fa9df33e82998fad09adde9e
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 10:57:02 -0500

    Add userproject service
    Created UserProject service and interface

---
+++ BugTracker.Core/Services/UserProjectService.cs
@@ -0,0 +1,56 @@
+﻿using BugTracker.Core.Interfaces;
+using BugTracker.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Core.Services
+{
+    public class UserProjectService : IUserProjectService
+    {
+        private readonly IUserProjectRepository _userProjectRepo;
+
+        public UserProjectService(IUserProjectRepository userProjectRepo)
+        {
+            _userProjectRepo = userProjectRepo;
+        }
+
+        public async Task<int> Create(UserProject entity)
+        {
+            return await _userProjectRepo.Create(entity);
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            return await _userProjectRepo.Delete(id);
+        }
+
+        public async Task<UserProject> FindOne(int id)
+        {
+            return await _userProjectRepo.FindOne(id);
+        }
+
+        public async Task<IEnumerable<UserProject>> GetAll()
+        {
+            return await _userProjectRepo.GetAll();
+        }
+
+        public async Task<IEnumerable<UserProject>> GetAllByUserId(int id)
+        {
+            var userProjects = await _userProjectRepo.GetAll();
+            userProjects = userProjects.Where(x => x.AppUserId == id);
+
+            if (userProjects == null)
+                return new List<UserProject>();
+
+            return userProjects;
+        }
+
+        public async Task<int> Update(UserProject entity)
+        {
+            return await _userProjectRepo.Update(entity);
+        }
+    }
+}
