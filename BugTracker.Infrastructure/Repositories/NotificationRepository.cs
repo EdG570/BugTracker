63ffa70cc4830c4bbb195da17f8d79cbe489d2c5
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 10:08:22 -0500

    Add notification repository
    Created notification repository interface and repo

---
+++ BugTracker.Infrastructure/Repositories/NotificationRepository.cs
@@ -0,0 +1,62 @@
+﻿using BugTracker.Core.Interfaces;
+using BugTracker.Core.Models;
+using BugTracker.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Infrastructure.Repositories
+{
+    public class NotificationRepository : INotificationRepository
+    {
+        private readonly IdentityAppContext _context;
+
+        public NotificationRepository(IdentityAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Create(Notification entity)
+        {
+            await _context.Notifications.AddAsync(entity);
+            await SaveChanges();
+
+            return 1;
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            var notification = await _context.Notifications.FindAsync(id);
+            _context.Notifications.Remove(notification);
+            await SaveChanges();
+
+            return 1;
+        }
+
+        public async Task<Notification> FindOne(int id)
+        {
+            return await _context.Notifications.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Notification>> GetAll()
+        {
+            return await _context.Notifications.ToListAsync();
+        }
+
+        public async Task SaveChanges()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Update(Notification entity)
+        {
+            var notification = await _context.Notifications.FindAsync(entity.Id);
+            _context.Entry(notification).CurrentValues.SetValues(entity);
+            await SaveChanges();
+
+            return 1;
+        }
+    }
+}
