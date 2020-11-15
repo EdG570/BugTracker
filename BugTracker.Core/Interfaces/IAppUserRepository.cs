5e612b624ec331367983b7df231fcaa86b4a31a1
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 10:21:35 -0500

    Add appuser repository
    Created Appuser repository and interface for data access

---
+++ BugTracker.Core/Interfaces/IAppUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using BugTracker.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Core.Interfaces
+{
+    public interface IAppUserRepository
+    {
+        Task<AppUser> FindOne(int id);
+    }
+}
---
+++ BugTracker.Infrastructure/Repositories/AppUserRepository.cs
@@ -0,0 +1,30 @@
+﻿using BugTracker.Core.Interfaces;
+using BugTracker.Core.Models;
+using BugTracker.Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Infrastructure.Repositories
+{
+    public class AppUserRepository : IAppUserRepository
+    {
+        private readonly IdentityAppContext _context;
+
+        public AppUserRepository(IdentityAppContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<AppUser> FindOne(int id)
+        {
+            return await _context.AppUsers.Include(x => x.UserProjects)
+                                          .ThenInclude(x => x.Project)
+                                          .ThenInclude(x => x.Tickets)
+                                          .ThenInclude(x => x.Comments)
+                                          .FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
