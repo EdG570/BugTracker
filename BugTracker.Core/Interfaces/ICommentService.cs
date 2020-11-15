c47688c33f91b5c9d5915bf2cc56ff542ff6faf9
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 10:31:09 -0500

    Add comment service
    Created comment service and interface

---
+++ BugTracker.Core/Interfaces/ICommentService.cs
@@ -0,0 +1,11 @@
+﻿using BugTracker.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BugTracker.Core.Interfaces
+{
+    public interface ICommentService : IGenericService<Comment>
+    {
+    }
+}
---
+++ BugTracker.Core/Services/CommentService.cs
@@ -0,0 +1,44 @@
+﻿using BugTracker.Core.Interfaces;
+using BugTracker.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BugTracker.Core.Services
+{
+    public class CommentService : ICommentService
+    {
+        private readonly ICommentRepository _commentRepo;
+
+        public CommentService(ICommentRepository commentRepo)
+        {
+            _commentRepo = commentRepo;
+        }
+
+        public async Task<int> Create(Comment entity)
+        {
+            return await _commentRepo.Create(entity);
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            return await _commentRepo.Delete(id);
+        }
+
+        public async Task<Comment> FindOne(int id)
+        {
+            return await _commentRepo.FindOne(id);
+        }
+
+        public async Task<IEnumerable<Comment>> GetAll()
+        {
+            return await _commentRepo.GetAll();
+        }
+
+        public async Task<int> Update(Comment entity)
+        {
+            return await _commentRepo.Update(entity);
+        }
+    }
+}
