f8a608f0b7bcdcf50ad4235010625e07339e8cea
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 09:41:53 -0500

    Add notification model

---
+++ BugTracker.Core/Models/Notification.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace BugTracker.Core.Models
+{
+    public class Notification
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+        public string Link { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public bool IsAcknowleged { get; set; } = false;
+
+        public int AppUserId { get; set; }
+        public virtual AppUser AppUser { get; set; }
+    }
+}
