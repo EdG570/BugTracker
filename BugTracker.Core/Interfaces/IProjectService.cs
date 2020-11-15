799e4e0d5c67e95cadff22293cd520211ea956f5
Ed Glazenski <eglazenski@gmail.com>
2020-11-15 10:44:02 -0500

    Add project service and interface

---
+++ BugTracker.Core/Interfaces/IProjectService.cs
@@ -0,0 +1,14 @@
+﻿using BugTracker.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BugTracker.Core.Interfaces
+{
+    public interface IProjectService : IGenericService<Project>
+    {
+        Task<IEnumerable<SelectListItem>> GetUsersAsSelectListItemsByProjectId(int id);
+    }
+}
---
+++ BugTracker.Core/Services/ProjectService.cs
@@ -0,0 +1,65 @@
+﻿using BugTracker.Core.Interfaces;
+using BugTracker.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BugTracker.Core.Services
+{
+    public class ProjectService : IProjectService
+    {
+        private readonly IProjectRepository _projectRepo;
+
+        public ProjectService(IProjectRepository projectRepo)
+        {
+            _projectRepo = projectRepo;
+        }
+
+        public async Task<int> Create(Project entity)
+        {
+            return await _projectRepo.Create(entity);
+        }
+
+        public async Task<int> Delete(int id)
+        {
+            return await _projectRepo.Delete(id);
+        }
+
+        public async Task<Project> FindOne(int id)
+        {
+            return await _projectRepo.FindOne(id);
+        }
+
+        public async Task<IEnumerable<Project>> GetAll()
+        {
+            return await _projectRepo.GetAll();
+        }
+
+        public async Task<IEnumerable<SelectListItem>> GetUsersAsSelectListItemsByProjectId(int id)
+        {
+            var project = await _projectRepo.FindOne(id);
+            var list = new List<SelectListItem>();
+
+            foreach (var userProject in project.UserProjects)
+            {
+                list.Add(new SelectListItem
+                {
+                    Value = userProject.AppUserId.ToString(),
+                    Text = userProject.AppUser.LastName.ToString() + ", " + userProject.AppUser.FirstName.ToString()
+                });
+            }
+
+            list = list.OrderBy(x => x.Text).ToList();
+
+            return list.Prepend(new SelectListItem { Value = "", Text = "Unassigned" });
+        }
+
+        public async Task<int> Update(Project entity)
+        {
+            return await _projectRepo.Update(entity);
+        }
+    }
+}
